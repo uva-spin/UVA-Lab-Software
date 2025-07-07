19d0a70da80cb850619aec4f10ba45755b8c18f9
PT Group <ptgroup.git@gmail.com>
2025-07-07 13:46:10 -0400

    Updating paths

--- GUI-Webserver/data_acquisition/config.py
+++ GUI-Webserver/data_acquisition/config.py
@@ -3,7 +3,7 @@
 # Remote Flask server configuration
 REMOTE_SERVER_URL = "http://128.143.231.224:5000"  # Flask server's IP and port
 
-TWIST_PATH = "/www.twist.phys.virginia.edu/www/spin/"
+TWIST_PATH = "/mnt/twist/www/spin/"
 
 DATA_PATH = f"{REMOTE_SERVER_URL}/data"
 
@@ -35,7 +35,7 @@ LOG_FILE = "data_acquisition.log"
 
 
 ### Database settings
-DATABASE_DIR = f"/www.twist.phys.virginia.edu/www/spin/instance"
+DATABASE_DIR = f"/mnt/twist/www/spin/instance"
 
 DATABASE_NAME = "flaskr.sqlite"
 
