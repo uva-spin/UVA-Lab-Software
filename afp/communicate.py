ccc8ee798d2789e35e5c25144536dc16b9a73811
LabUser <LabUser@PHYS-8T6WYV1.eservices.virginia.edu>
2026-01-06 16:27:55 -0500

    .\__pycache__\

--- afp/communicate.py
+++ afp/communicate.py
@@ -6,20 +6,41 @@ if not instr_list:
     exit()
 print(instr_list)
 
-isntr = RsInstrument('GPIB::28:INSTR', True, True, "SelectVisa='rs'")
+instr = RsInstrument('USB0::0x0AAD::0x0048::101548::INSTR', True, True,options='TerminationCharacter = \r\n')
+idn = instr.query_str('*IDN?')
+print(f"\nHello, I am: '{idn}'")
 
-print(isntr.check_status())
+print(f'RsInstrument driver version: {instr.driver_version}')
 
-print(isntr.query('*IDN?'))
-print(isntr.full_instrument_model_name())
-isntr.close()
+print(f'Visa manufacturer: {instr.visa_manufacturer}')
 
-# isntr.write_float('CONF:VOLT:DC', 10)
-# isntr.write_float('CONF:CURR:DC', 1)
-# isntr.write_float('CONF:FREQ', 1000)
+print(f'Instrument full name: {instr.full_instrument_model_name}')
 
-# print(isntr.query_float('CONF:VOLT:DC?'))
-# print(isntr.query_float('CONF:CURR:DC?'))
-# print(isntr.query_float('CONF:FREQ?'))
+print(f'Instrument installed options: {",".join(instr.instrument_options)}')
 
+instr.visa_timeout = 3000
+instr.write('*RST')
+# instr.write('SOURCE:FREQUENCY:CW 213MHz')
+instr.write('SOURCE:FREQuency:START 212MHz')
+instr.write('SOURCE:FREQuency:STOP 214MHz')
+instr.write('SOURCE:SWEEP:SPACING LINear')
+# instr.write('SOURCE:FUNCTION:SHAPE SIN')
+# instr.write('SOURCE:FREQ:STEP 10kHz')
+instr.write('SOURCE:SWEEP:POINTS 500')
+instr.write('SOURCE:POWER:POWER 12')
+instr.write('SOURCE:FREQUENCY:MODE SWEep')
+instr.write('SOURCE:SWEEP:DWELl 10ms')
+instr.write('SOURCE:SWEEP:MODE AUTO')
+instr.write('OUTPUT:STATE ON')
 
+freq = instr.query_float('SOURCE:FREQUENCY:CW?')
+power = instr.query_float('SOURCE:POWER:POWER?')
+output = instr.query_bool('OUTPUT:STATE?')
+sweep_points = instr.query_int('SOURCE:SWEEP:POINTS?')
+
+print(f'Frequency: {freq}')
+print(f'Power: {power} dBm')
+print(f'Output: {output}')
+print(f'Sweep points: {sweep_points}')
+
+instr.close()
